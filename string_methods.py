828fce6ae3beb78568591f1c9a78b08550318bf3
justoestevez <jeestevez@mail.austral.edu.ar>
2026-03-17 19:12:56 -0300

    Update string_methods.py

--- string_methods.py
+++ string_methods.py
@@ -9,25 +9,26 @@ def string_methods():
     Linea 2
     Linea 3"""
 
-   print(f"Strip: {nombre.strip()}")
+    print (f"Strip: {nombre.strip()}")
     print(f"Lstrip: {nombre.lstrip()}")
     print(f"Rstrip: {nombre.rstrip()}")
 
-    print(f"Upper: {frase.upper()}")
-    print(f"Lower: {frase.lower()}")
-    print(f"Title: {frase.title()}")
+    print (f"Upper: {frase.upper()}")
+    print (f"Lower: {frase.lower()}")
+    print (f"Title: {frase.title()}")
 
-    print(f"Find: {frase.find('gran')}")
+    print (f"Find: {frase.find('gran')}")
     print(f"Replace: {frase.replace('programacion', 'desarrollo')}")
     print(f"Count: {frase.count('a')}")
 
     print(f"Contiene Python: {'Python' in frase}")
     print(f"Contiene Java: {'Java' in frase}")
 
-    print(f"Slice: {frase[0:6]}")
-    print(f"Paso: {frase[0:6:2]}")
-    print(f"Reverso: {frase[5::-1]}")
+    print(f"Slice: {frase[0:6]}") 
+    print(f"Paso: {frase[0:6:2]}") 
+    print(f"Reverso: {frase[5::-1]}") 
 
-    print(f"Formato: {nombre.strip()} sabe Python")
+    print(f"Formato: {nombre.strip() + ' '+'sabe' +' '+palabra}")
     print(multilinea)
+
 
